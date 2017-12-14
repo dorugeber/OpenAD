fe9983fa754d19f34736772afae6a8fb3323653f
Andrew McRae <andrew.mcrae@physics.ox.ac.uk>
2017-12-14 00:53:07 +0000

    make changes for toy RPE examples

--- Examples/OneMinuteRPE/driver.f90
+++ Examples/OneMinuteRPE/driver.f90
@@ -1,10 +1,18 @@
 program driver
   use OAD_active
+  use rp_emulator
   implicit none 
   external head
   type(active) :: x, y
+
+! INITIALIZATION EMULATOR
+  RPE_ACTIVE = .TRUE.
+  RPE_DEFAULT_SBITS = 5
+
   x%v=.5D0
   x%d=1.0D0
+  call zero_deriv(y)
+
   call head(x,y)
   print *, 'driver running for x =',x%v
   print *, '            yields y =',y%v,' dy/dx =',y%d
--- Examples/OneMinuteReverseRPE/driver.f90
+++ Examples/OneMinuteReverseRPE/driver.f90
@@ -1,11 +1,19 @@
 program driver
   use OAD_active
   use OAD_rev
+  use rp_emulator
   implicit none 
   external head
   type(active) :: x, y
+
+! INITIALIZATION EMULATOR
+  RPE_ACTIVE = .TRUE.
+  RPE_DEFAULT_SBITS = 5
+
   x%v=.5D0
   y%d=1.0D0
+  call zero_deriv(x)
+
   our_rev_mode%tape=.TRUE.
   call head(x,y)
   print *, 'driver running for x =',x%v
